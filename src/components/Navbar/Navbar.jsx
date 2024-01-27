c8dab5d78a63c29a17ea2b630dd0e2cd830a9594
Slayershrey <sawalkar336@gmail.com>
2024-01-27 08:55:50 +0530

    corrected some mistakes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
-import styles from "./Navbar.module.css";
-import { getImageUrl } from "../../utils";
+import styles from "./Navbar.module.css"
+import { getImageUrl } from "../../utils"
 
 export const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <nav className={styles.navbar}>
@@ -30,16 +30,16 @@ export const Navbar = () => {
             <a href="#about">About</a>
           </li>
           <li>
-            <a href="#experience">Experience</a>
+            <a href="#experience">Skills</a>
           </li>
           <li>
             <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#about">Contact</a>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </div>
     </nav>
-  );
-};
+  )
+}
