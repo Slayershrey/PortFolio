2fd9c502f511f8fde7e69a76a5d932a5463528a4
Slayershrey <sawalkar336@gmail.com>
2024-01-30 12:26:33 +0530

    Added Typing Effect

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,15 +2,18 @@ import React from "react"
 
 import styles from "./Hero.module.css"
 import { getImageUrl } from "../../utils"
+import { useTypingEffect } from "../../useTypingEffect"
 
 export const Hero = () => {
+  const text = useTypingEffect(
+    "A passionate Programmer, Developer and Problem Solver",
+    200
+  )
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Shreyansh Sawalkar</h1>
-        <p className={styles.description}>
-          A passionate Programmer, Developer and Problem Solver
-        </p>
+        <p className={styles.description}>{text}</p>
         <a href="mailto:sawalkar336@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
